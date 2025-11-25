c98e0ed169d25ca018a64a61bef55fb56451daa5
ramyaaa28 <157025019+ramyaaa28@users.noreply.github.com>
2025-11-25 11:30:02 +0530

    Fix vite config for Vercel

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "./",   // <--- THIS FIXES THE WHITE SCREEN
+  base: "./",
+  build: {
+    outDir: "dist",
+  }
 })
