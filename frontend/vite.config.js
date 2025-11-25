b8a3309edfb7d240c70949b34e567421ca2b57b6
ramyaaa28 <157025019+ramyaaa28@users.noreply.github.com>
2025-11-25 13:46:02 +0530

    Fix API URL for Vercel

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,8 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "",  
+  server: {
+    port: 5173
+  },
+  preview: {
+    port: 4173
+  },
   build: {
-    outDir: "dist",
+    outDir: 'dist'
   }
 })
